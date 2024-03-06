d2061cd69654233949b2fa4b9e40c81f1a191140
PrayasPathak <prayasroman1@gmail.com>
2024-03-06 17:43:12 +0545

    models and admin setup

--- linkplant/admin.py
+++ linkplant/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from .models import Profile, Link
 
 # Register your models here.
+
+
+class NameFilter(admin.SimpleListFilter):
+    title = "name"
+    parameter_name = "name"
+    ASC = "asc"
+    DESC = "desc"
+
+    def lookups(self, request, model_admin):
+        return [(self.ASC, "Asc"), (self.DESC, "Desc")]
+
+    def queryset(self, request, queryset):
+        if self.value() == self.ASC:
+            return queryset.order_by("name")
+        else:
+            return queryset.order_by("-name")
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ["name", "slug", "bg_color"]
+    list_per_page = 20
+    list_filter = ["id", NameFilter]
+    list_editable = ["bg_color"]
+    prepopulated_fields = {
+        "slug": ["name"],
+    }
+    search_fields = ["name"]
+
+
+@admin.register(Link)
+class LinkAdmin(admin.ModelAdmin):
+    list_display = ["text", "profile", "url"]
+    list_search = ["profile"]
+    list_per_page = 20
--- linkplant/models.py
+++ linkplant/models.py
@@ -2,3 +2,30 @@ from django.db import models
 
 
 # Create your models here.
+class Profile(models.Model):
+    BG_COLOR_YELLOW = "yellow"
+    BG_COLOR_BLUE = "blue"
+    BG_COLOR_GREEN = "green"
+
+    BG_CHOICES = (
+        (BG_COLOR_BLUE, "Blue"),
+        (BG_COLOR_YELLOW, "Yellow"),
+        (BG_COLOR_GREEN, "Green"),
+    )
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, max_length=100)
+    bg_color = models.CharField(
+        max_length=50, choices=BG_CHOICES, default=BG_COLOR_BLUE
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Link(models.Model):
+    text = models.CharField(max_length=255)
+    url = models.URLField()
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="links")
+
+    def __str__(self):
+        return f"{self.profile} | {self.url}"
